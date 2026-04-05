6237a55999a316fd1fdbc0904918889143172dc8
Sijuchan <sijus.uk@gmail.com>
2026-04-06 01:46:41 +0200

    Initial Commit

---
+++ APODDaily/APODDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODDailyApp.swift
+//  APODDaily
+//
+//  Created by Siju Satheesachandran on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct APODDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
